803adc5c51a7f523610ed5246729d6074b816f9f
Nicolas Chambrier <naholyr@gmail.com>
2011-10-24 01:28:06 +0200

    Encrypted password

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var connect = require('connect')
   , socketio = require('socket.io')
   , app = connect.createServer(connect.static(__dirname + '/public'))
   , sockets = socketio.listen(app).sockets
+  , password = ['sha256', 'a901d03d36717cdb3a75e9a0b85cecca480482ba46de44a3eaa2798b68420981']
 
 sockets.on('connection', function (client) {
   client
   .on('auth-controller', function (pass) {
-    if (pass == 'coucougamin') {
+    if (require('crypto').createHash(password[0]).update(pass).digest('hex') == password[1]) {
       client.controller = true
       client.emit('auth-controller.success')
     } else {
