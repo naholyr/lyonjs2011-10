01720f63d946e85935a32b244b5dfbd2d03fb4f5
Nicolas Chambrier <naholyr@gmail.com>
2011-10-23 23:40:22 +0200

    Initial commit (static files)

---
+++ main.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	// Deck initialization
+	$.deck('.slide');
+
+	// Enable syntax highlighting
+	(function (p) {
+		var old_getHtml = p.getHtml;
+		p.getHtml = function (code) {
+			var html = old_getHtml.call(this, code);
+			// Inherit from theme web-2.0
+			return '<pre class="syntaxhighlighter-container">' + html + '</pre>';
+		}
+	})(SyntaxHighlighter.Highlighter.prototype);
+	SyntaxHighlighter.defaults["gutter"] = false;
+	SyntaxHighlighter.defaults["toolbar"] = false;
+	SyntaxHighlighter.defaults["quick-code"] = false;
+	SyntaxHighlighter.all();
+
+	// Remote control
+
+});
+
